e4dc51347e68bc3a1f4b5edfe05df7920f4cd2ae
ShellHase <krieger.jacke@gmail.com>
2022-11-11 14:15:04 +0100

    Midified App.js to a default example app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      BikeForFuture
+    <View style={styles.mainContent}>
+      <Text style={{fontSize: 22}}>BikeForFuture</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  mainContent: {
+    height: "80%",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
