e951d428a444f88b7a501580044b6cb9aefc3434
echterPhilipp <Philipp.Spreckelse@web.de>
2022-12-20 18:41:24 +0100

    added example graph

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Pressable, Modal, Dimensions } from "react-native";
+import { StyleSheet, View, Text, Pressable, Modal, Dimensions, ScrollView } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { LineChart } from 'react-native-chart-kit'
 
 import Calendar from "./Calendar";
 
@@ -48,6 +49,52 @@ export default function Home() {
       </View>
       <View style={styles.mainContent}>
         <Text style={{ fontSize: 22 }}>BikeForFuture</Text>
+        <ScrollView>
+          <View style={styles.container}>
+            <View>
+              {/*It is an Example of LineChart*/}
+              <Text
+                style={{
+                  textAlign: 'center', fontSize: 18,
+                  padding: 16,
+                  marginTop: 16,
+                }}>
+                Line Chart
+              </Text>
+              <LineChart data={{
+                labels: [ '5',
+                '10',
+                '15',
+                '20',
+                '25',
+                '30',
+                ],
+                datasets: [
+                {
+                data: [40, 60, 10, 80, 99, 43],
+                strokeWidth: 2,
+                },
+                ],
+                }}
+                width={Dimensions.get('window').width - 16}
+                height={400}
+                chartConfig={{
+                backgroundColor: '#9f1414',
+                backgroundGradientFrom: '#fac82f',
+                backgroundGradientTo: '#ff8302',
+                decimalPlaces: 2,
+                color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                borderRadius: 16,
+                },
+                }}
+                style={{ marginVertical: 8,
+                borderRadius: 16,
+                }}
+              />
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
